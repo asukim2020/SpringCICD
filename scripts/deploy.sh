53725fb01e1737179a5ac07c704ec4cf5522e120
asukim2020 <63150458+asukim2020@users.noreply.github.com>
2021-12-20 11:50:40 +0900

    CICD 테스트 016

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,20 +1,20 @@
 REPOSITORY=/home/ec2-user/app/SpringCICD
-cd $REPOSITORY
+#cd $REPOSITORY
 
-APP_NAME=SpringCICD #1
+APP_NAME=SpringCICD
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
-if [ -z $CURRENT_PID ] #2
+if [ -z $CURRENT_PID ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
   sudo kill -15 $CURRENT_PID
-  sleep 5
+  sleep 10
 fi
 
-echo "> $JAR_PATH 배포" #3
+echo "> $JAR_PATH 배포"
 nohup java -jar /home/ec2-user/app/SpringCICD/build/libs/SpringCICD-0.1.jar
